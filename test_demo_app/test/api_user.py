2f913923e799b46c588ebac8cb3a67c4be55499d
Lopezmartha <idoghormat@gmail.com>
2022-09-02 18:15:42 +0300

    updated

--- test_demo_app/test/api_user.py
+++ test_demo_app/test/api_user.py
@@ -3,8 +3,7 @@ import string
 import json
 host_url = 'http://127.0.0.1:8080'
 headers = {
-    'Content-Type': 'application/json',
-    'Token': 'MjEyNDkwMTkzMDcxNTEzMTI1ODAzOTczNzM1NjM4MDgwNTkzNDAw'
+    'Content-Type': 'application/json'
 }
 headers1 = {
   'Content-Type': 'application/json'
@@ -14,10 +13,15 @@ allowed_fields = (
     'lastname',
     'phone'
 )
+
+''' 
+1. Register new users
+'''
+
 def test_post_register_new_user():
     url = host_url + '/api/users'
     new_user = json.dumps({
-                'username': 'tim23',
+                'username': 'tim231',
                 'password': '1234',
                 'firstname': 'tim',
                 'lastname': 'run',
@@ -25,8 +29,11 @@ def test_post_register_new_user():
                 })
     response = requests.request('POST', url, headers=headers1, data=new_user)
     response = response.status_code
-    assert_request_created_success(response)
+    assert_request_created_successfully(response)
 
+'''
+2. Review users registered in system
+'''
 
 def test_get_all_user_list():
     url = host_url + '/api/users'
@@ -39,15 +46,6 @@ def test_get_all_user_list():
 
 
 
-def assert_request_created_success(response):
+def assert_request_created_successfully(response):
     assert response == 201
-
-def assert_field_values_from_response(response, value):
-    json_response = response.json()
-    for field in allowed_fields:
-        assert json_response['payload'][field] == value
-
-def assert_request_response_success(response):
-    json_response = response.json()
-    assert json_response['status'] == 'SUCCESS'
 
